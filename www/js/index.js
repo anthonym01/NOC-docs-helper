fba3f93e4e4bb115174f00c96ea1b772add01256
Samuel A. Matheson <samuelmatheson15@gmail.com>
2022-11-23 17:55:35 -0500

    lets gooooooo

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('load', function () {
-    
+
 });
 
 //strip variables from links
@@ -21,6 +21,30 @@ function get_url_variables(url) {//Gets url variables as an object
     //returns Object { "": "undefined" } if empty
     //Call with var this_url = getParams(window.location.href);
 }
+
+let tickets = {
+    initalize: async function () {
+
+    },
+    actionlevels: [
+        "No aplicable action",// clear - 0
+        "Add note to ticket. Anounce to slack. find an engineer to take ownership.",//Red - 1
+        "Put a note in the ticket. Assign to yourself. Update Ticket every 2 hours with current status. If no customer response after 3 checks, escalate to management for follow-up",//yellow - 2
+        "Check with IBM Team",//purple - 3
+        "Add note to ticket. Announce to Slack. Leave unnasigned",//blue - 4
+        "Leave alone",//green - 5
+        "Add note to ticket. Leave unassigned",//Dark blue - 6
+    ],
+    actionmatrixday: [//yes im faking natural language, sue me
+        { typename: "Customer generated: Incident",tokens:"INC incident Customer generated", prioritymatrix: [1, 1, 4, 4] },
+        { typename: "Customer generated: Change request",tokens:"CHG change request Customer generated", prioritymatrix: [1, 1, 4, 4] },
+        { typename: "Customer generated: RITM",tokens:"RITM new user login logging SNOW", prioritymatrix: [1, 4, 4, 4] },
+        { typename: "Customer generated: IBM VA",tokens:"IBM VA Veteran Veterans association", prioritymatrix: [1, 3, 4, 4] },
+        { typename: "Customer generated: IBM GSA",tokens:"IBM GSA", prioritymatrix: [1, 3, 4, 4] },
+        { typename: "Customer generated: IBM NELLC",tokens:"IBM NELLC", prioritymatrix: [1, 3, 4, 4] },
+    ],
+}
+
 
 /* Testing/junk/examples */
 
